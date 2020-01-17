f29d7b57b9df0553ed9aae2b0da9c1c94e5c2cca
rinish06 <54551163+rinish06@users.noreply.github.com>
2020-01-17 12:35:19 -0600

    Teammate changes
    Added Circular pickup
    Added shades to the pickup, wall, ground

---
+++ Assets/Scripts/Circular.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Circular : MonoBehaviour
+{
+
+    public float speed = Mathf.PI/10;
+    private float radius = 4.0f;
+    private float a = 0;
+    private float x = 3.0f;
+    private float z = 3.0f;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        a += speed * Time.deltaTime;
+        
+        x = radius * Mathf.Sin(a) ;
+        z = radius * Mathf.Cos(a)  ;
+
+        transform.position = new Vector3(x, 0.5f, z);
+
+    }
+}
