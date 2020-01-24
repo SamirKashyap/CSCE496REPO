bfc6bf4fb1a2b537e285515c49a0c99790af2bf4
Samir Kashyap <samir.kashyap17@gmail.com>
2020-01-24 17:01:38 -0600

    Epic Gamer Moment

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,49 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour
+
+namespace Valve.VR.InteractionSystem
 {
-    public float speed;
-    public Text countText;
-    public Text winText;
-
-    private Rigidbody rb;
-    private int count;
-
-    void Start()
+    public class PlayerController : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody>();
-        count = 0;
-        winText.text = "";
-        SetCountText();
-    }
+        public float speed;
+        public Text countText;
+        public Text winText;
 
-    void SetCountText()
-    {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 9)
+        private Rigidbody rb;
+        public int count;
+
+        void Start()
         {
-            winText.text = "You win :)";
-        }
-    }
-
-    void FixedUpdate()
-    {
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-        float moveVertical = Input.GetAxis ("Vertical");
-
-        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-
-        rb.AddForce (movement * speed);
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Pick Up"))
-        {
-            other.gameObject.SetActive(false);
-            count += 1;
+            rb = GetComponent<Rigidbody>();
+            count = 0;
+            winText.text = "";
             SetCountText();
+        }
+
+        void SetCountText()
+        {
+            countText.text = "Count: " + count.ToString();
+            if (count >= 9)
+            {
+                winText.text = "You win :)";
+            }
+        }
+
+        void FixedUpdate()
+        {
+            //    float moveHorizontal = Input.GetAxis ("Horizontal");
+            //    float moveVertical = Input.GetAxis ("Vertical");
+
+            //    Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+            //    rb.AddForce (movement * speed);
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Pick Up"))
+            {
+                other.gameObject.SetActive(false);
+                count += 1;
+                SetCountText();
+            }
         }
     }
 }
